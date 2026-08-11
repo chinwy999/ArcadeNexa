1353dbbe4b54731a7026cd0212dfb7b7cf1ce8df
chinwy999 <bensasimustapha5@gmail.com>
2026-08-11 16:59:39 +0100

    fix: images config and enable dynamic feed

--- next.config.mjs
+++ next.config.mjs
@@ -6,6 +6,18 @@ const nextConfig = {
   eslint: {
     ignoreDuringBuilds: true,
   },
+  images: {
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: '**',
+      },
+      {
+        protocol: 'http',
+        hostname: '**',
+      },
+    ],
+  },
 };
 
 export default nextConfig;
