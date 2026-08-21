73833c5b54ab3f917d0d3fb3778381e902be7153
chinwy999 <bensasimustapha5@gmail.com>
2026-08-22 00:36:22 +0100

    Remove obsolete news routes

--- app/news/[slug]/page.tsx
+++
@@ -1,9 +0,0 @@
-import { permanentRedirect } from 'next/navigation'
-
-export default function NewsArticleRedirectPage({
-  params,
-}: {
-  params: { slug: string }
-}) {
-  permanentRedirect(`/blog/${params.slug}`)
-}
