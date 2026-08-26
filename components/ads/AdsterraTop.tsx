4fc1f8989f3256c0754391abf6500a0b7d771506
chinwy999 <bensasimustapha5@gmail.com>
2026-08-27 00:25:39 +0100

    Set desktop top ad to 728x90

--- components/ads/AdsterraTop.tsx
+++ components/ads/AdsterraTop.tsx
@@ -5,18 +5,28 @@ import Script from 'next/script';
 export default function AdsterraTop() {
   return (
     <div className="w-full px-3 sm:px-4">
-      <div
-        className="
-          mx-auto my-5 flex min-h-[50px] w-full
-          max-w-7xl items-center justify-center
-          overflow-hidden
-          rounded-lg
-        "
-      >
-        <Script
-          src="https://pl31045270.profitableratecpmnetwork.com/a1/dd/b8/a1ddb888e0b6ea30ae8110f682aa63d0.js"
-          strategy="afterInteractive"
-        />
+      <div className="mx-auto my-5 flex min-h-[90px] w-full items-center justify-center overflow-hidden">
+        <div className="flex h-[90px] w-full max-w-[728px] items-center justify-center">
+          <Script
+            id="adsterra-desktop-728x90-options"
+            strategy="afterInteractive"
+          >
+            {`
+              atOptions = {
+                'key': 'f13d3677e8c7d620f4f19e175fdc9782',
+                'format': 'iframe',
+                'height': 90,
+                'width': 728,
+                'params': {}
+              };
+            `}
+          </Script>
+
+          <Script
+            src="https://www.highrevenueformat.com/f13d3677e8c7d620f4f19e175fdc9782/invoke.js"
+            strategy="afterInteractive"
+          />
+        </div>
       </div>
     </div>
   );
