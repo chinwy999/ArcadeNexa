ebfc8e7afc85799a5cad6c929d746556a45588fc
chinwy999 <bensasimustapha5@gmail.com>
2026-09-18 21:19:09 +0100

    Cache Chromebook games page

--- app/chromebook-games/page.tsx
+++ app/chromebook-games/page.tsx
@@ -1,5 +1,5 @@
 import type { Metadata } from 'next'
-export const dynamic = 'force-dynamic'
+export const revalidate = 3600
 import Link from 'next/link'
 import { getGames } from '@/lib/games'
 import SafeImage from '@/components/SafeImage'
